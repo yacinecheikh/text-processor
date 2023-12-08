29ae614d6d6b4599df41e23459ca8d6b2ac039df
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2023-12-08 14:38:28 +0100

    WIP: finished rewriting parse.rs again

--- src/parse.rs
+++ src/parse.rs
@@ -1,10 +1,23 @@
-use crate::parse::Command::{Block, Oneliner};
+
+// syntaxes:
+// .command:\n (waiting for a block, indented or not)
+// .command: arg\n (may have a block, or not)
+// .command\n (not waiting for a block)
+struct Tag {
+    name: String,
+    accept_block: bool, // true if ":" was found before the end of line
+    base_indent: String, // indentation at the head line
+    argument: Option<String>, // if an argument is found, having a block after : is optional and requires further indentation
+}
+struct ReplaceBlock {
+    name: String,
+    argument: Option<String>,
+    body: Option<String>,
+}
+
+// TODO: remove
 
 enum CallHeader {
-    // syntaxes:
-    // .command:\n (waiting for a block, indented or not)
-    // .command: arg\n (may have a block, or not)
-    // .command\n (not waiting for a block)
     Block {
         name: String,
         base_indent: String,
@@ -20,13 +33,6 @@ enum CallHeader {
     }
 }
 
-// TODO: remove
-struct CommandLine {
-    // first line of what can be an indented block
-    base_indent: String,
-    name: String,
-    argument: Option<String>,
-}
 
 struct Call {
     name: String,
@@ -34,20 +40,6 @@ struct Call {
     argument: Option<String>,
 }
 
-// TODO: remove
-enum Command {
-    // completely parsed call
-    Oneliner {
-        name: String,
-        arg: Option<String>,
-    },
-    Block {
-        name: String,
-        arg: Option<String>,
-        content: String,
-        // indent ?
-    },
-}
 
 
 /* generic string parsing utilities*/
@@ -58,9 +50,9 @@ fn split(text: &str, offset: usize) -> (&str, &str) {
 }
 
 
-pub fn split_indent(text: &str) -> (&str, &str) {
+pub fn strip_indent(line: &str) -> (&str, &str) {
     let mut offset = 0;
-    for ch in text.chars() {
+    for ch in line.chars() {
         match ch {
             ' ' | '\t' => {
                 offset += ch.len_utf8();
@@ -70,17 +62,17 @@ pub fn split_indent(text: &str) -> (&str, &str) {
             }
         }
     }
-    split(text, offset)
+    split(line, offset)
 }
 
 
-pub fn split_line(text: &str) -> Option<(&str, &str)> {
+pub fn strip_line(text: &str) -> Option<(&str, &str)> {
     let mut offset = 0;
     for ch in text.chars() {
         match ch {
             '\n' => {
                 // the \n should be removed, but this requires testing
-                offset += ch.len_utf8();
+                //offset += ch.len_utf8();
                 break
             }
 
@@ -101,16 +93,17 @@ fn is_empty(line: &str) -> bool {
     for ch in line.chars() {
         match ch {
             ' ' | '\t' => {},
-            '\n' => return true,
+            // TODO: remove \n at end of line
+            '\n' => return true, // special case because i may have left a \n at the end of each line
             _ => return false,
         }
     }
     return true
 }
 
-pub fn split_empty_lines(mut text: &str) -> (&str, &str) {
+pub fn strip_empty_lines(mut text: &str) -> (&str, &str) {
     let mut offset = 0;
-    // only change after a complete line is parsed
+    // only commit changes after a complete line is parsed
     let mut committed_offset = 0;
     for ch in text.chars() {
         match ch {
@@ -126,150 +119,137 @@ pub fn split_empty_lines(mut text: &str) -> (&str, &str) {
             }
         }
     }
-    split(text, committed_offset)
+    if text.as_bytes().len() == offset {
+        // all the text is blank lines
+        split(text, offset)
+    } else {
+        split(text, committed_offset)
+    }
+}
+
+/* syntax-specific parsing */
+
+fn get_next_indent(text: &str) -> Option<String> {
+    let (empty_lines, rest) = strip_empty_lines(text);
+    match strip_line(rest) {
+        None => {
+            // no line to add in the block
+            None
+        }
+        Some((line, _)) => {
+            let (indent, _) = strip_indent(line);
+            return Some(indent.to_string())
+        }
+    }
 }
 
 static DELIMITER: &str = ".";
 
-pub fn parse_header(line: &str) -> Option<CallHeader> {
-    let (indent, line) = split_indent(line);
+fn parse_argument(s: &str) -> Option<String> {
+    if s.len() == 0 {
+        None
+    } else {
+        // optional space after ":"
+        let s = s.strip_prefix(" ").unwrap_or(s);
+        Some(s.to_string())
+    }
+}
+
+fn parse_tag(line: &str) -> Option<Tag> {
+    // parse lines that look like this: .{command}[:[ [{argument}]]]
+    let (indent, line) = strip_indent(line);
     if !line.starts_with(DELIMITER) {
         return None
     }
+    let indent = indent.to_string();
     let (_, line) = split(line, DELIMITER.as_bytes().len());
 
-    if !line.contains(":") {
-        return Some(CallHeader::Oneliner {
-            // TODO: strip right ? (or at least remove the \n if still here)
+    if !line.contains(":") { // simplest case
+        Some(Tag {
             name: line.to_string(),
+            accept_block: false,
+            base_indent: indent,
+            argument: None,
+        })
+    } else {
+        let (begin, end) = line.split_once(":").unwrap();
+        Some(Tag {
+            name: begin.to_string(),
+            accept_block: true,
+            base_indent: indent,
+            argument: parse_argument(end),
         })
     }
-    let (start, end) = line.split_once(":").unwrap();
-    // TODO: remove the trailing \n in split_line
-    if end.len() == 0 {
-        return Some(CallHeader::Block {
-            name: start.to_string(),
-            base_indent: indent.to_string(),
-        })
-    }
-    // support both ".title: Title" and ".title:Title"
-    let end = end.strip_prefix(" ").unwrap_or(end);
-    return Some(CallHeader::Mixed {
-        name: start.to_string(),
-        base_indent: indent.to_string(),
-        argument: end.to_string(),
-    })
 }
 
 
 
-pub fn parse_command(header: CallHeader, text: &str) -> Result<(Call, &str), String> {
-    return match header {
-        CallHeader::Oneliner {
-            name
-        } => {
-            // no block to parse
-            Ok((Call {
-                name,
-                body: None,
-                argument: None,
-            }, text))
-        }
-        CallHeader::Block {
-            name,
-            base_indent
-        } => {
-            match parse_block(text, Some(base_indent.as_str())) {
-                None => {
-                    println!("WARNING: could not find a valid block after '.{}:' (using placeholder instead)", name);
-                    Ok((Call {
-                        name,
-                        body: Some("".to_string()),
-                        argument: None,
-                    }, text))
-                }
-                Some((content, text)) => {
-                    Ok((Call {
-                        name,
-                        body: Some(content),
-                        argument: None,
-                    }, text))
-                }
-            }
-        }
-        CallHeader::Mixed {
-            name, base_indent, argument
-        } => {
-            match parse_block(text, if base_indent.len() == 0 { None } else { Some(base_indent.as_str()) }) {
-                None => {
-                    // no block
-                    Ok((Call {
-                        name,
-                        body: None,
-                        argument: Some(argument),
-                    }, text))
-                }
-                Some((content, text)) => {
-                    Ok((Call {
-                        name,
-                        body: Some(content),
-                        argument: Some(argument),
-                    }, text))
-                }
-            }
-        }
-    }
-}
-
-
-// TODO: this code is too complex because of me trying to guess the indent level while parsing
-//  (instead of parsing two times to separate tasks)
-fn parse_block<'a>(text: &'a str, base_indent: Option<&str>) -> Option<(String, &'a str)>{
-    // find first indented line to get the indent level
-    let (empty, left) = split_empty_lines(text);
-    let result = split_line(left);
+fn parse_body<'a>(text: &'a str, tag: &Tag) -> Option<(String, &'a str)> {
+    let result = get_next_indent(text);
+    // all of this is just safety checks
     if result.is_none() {
-        // nothing to parse -> no block to return
-        // (for Block: make an empty block and print a warning)
-        // (TODO, not in this function)
+        // no body content was found
         return None
     }
-    let (first_line, left) = result.unwrap();
-    let (indent, line) = split_indent(first_line);
-    // nothing that can be parsed -> no block to return
-    // same as above
-    if let Some(base_indent) = base_indent {
-        if indent.starts_with(base_indent) {
-            // the first line does not belong to the block
-            // -> warning + empty block if needed (Block), None if not (Mixed)
-            return None
-        }
+    let indent = result.unwrap();
+    // check that the indentation level is correct
+    if !indent.starts_with(tag.base_indent.as_str()) {
+        return None
+    }
+    if tag.argument.is_some() && indent.len() == tag.base_indent.len() {
+        // using .tag:\n is the only case where using an empty indentation is accepted
+        // (in order to allow commands to process all the text, and one-line commands with a parameter)
+        return None
     }
 
-    // from this point, the first line either follows the previous indentation level,
-    // or there was no indentation before
-    let mut block: String = String::new();
-    block.push_str(empty);
-    block.push_str(first_line);
-    let mut text = left;
-    while true { // sorry
-        let (empty, left) = split_empty_lines(text);
-        match split_line(left) {
-            None => {
-                // end of block
-                return Some((block, text))
-            }
-            Some((full_line, left)) => {
-                let (indent, line) = split_indent(full_line);
-                if !indent.starts_with(base_indent) {
-                    // end of block
-                    return Some((block, text))
-                }
-                block.push_str(empty);
-                block.push_str(full_line);
-                text = left;
-            }
+    // at this point, there is at least one line that is correctly indented in the body
+    // so this should work
+    let mut body = String::new();
+    let mut text = text;
+    while text.len() > 0 {
+        let (empty, left) = strip_empty_lines(text);
+        let result = strip_line(left);
+        if result.is_none() {
+            // end of text
+            break
+        }
+        let (line, left) = result.unwrap();
+        let (line_indent, _) = strip_indent(line);
+        if line_indent.starts_with(indent.as_str()) {
+            // the line is still in the body
+            body.push_str(empty);
+            body.push_str(line);
+            body.push('\n');
+            text = left;
+        } else {
+            // not in the block anymore
+            break
         }
     }
-    None // never used
+    return Some((body, text));
+}
+
+fn parse_section(text: &str) -> Option<Result<(ReplaceBlock, &str), String>> {
+    let (line, text) = strip_line(text)?;
+    let tag = parse_tag(line)?;
+
+    let mut result = ReplaceBlock {
+        name: tag.name,
+        argument: tag.argument,
+        body: None,
+    };
+    let mut left_text = text;
+    match parse_body(left_text, &tag) {
+        None if tag.argument.is_none() => {
+            // Syntax error: expecting a body
+            return Some(Err(format!("Syntax error: expecting a body after \".{}:\"", tag.name)));
+        }
+        Some((body, text)) => {
+            left_text = text;
+            result.body = Some(body)
+        }
+        _ => {}
+    }
+
+    Some(Ok((result, left_text)))
 }
